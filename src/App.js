1da802e27368f93e488c6baf3b14daad13cbd5b0
Pierre <piercel.coder2022@gmail.com>
2022-08-29 17:27:57 +0200

    Add the workflow

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
+import Missions from './components/missions/missions';
+import Dragons from './components/dragons/dragons';
+import Rockets from './components/rockets/rocckets';
+import Profile from './components/profile/profile';
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+          <Route path="/Dragons" element={<Dragons />} />
+          <Route path="/Missions" element={<Missions />} />
+          <Route path="/Profile" element={<Profile />} />
+        </Routes>
+
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import applogo from '../images/planet.png';
+import './nav.css';
 
 function Nav() {
   return (
-    <div>
-      <div>
-        <img src={applogo} alt="space logo" width="28" height="28" />
-        <p>Space Travelers Hub</p>
+    <div className="header">
+      <div className="logo">
+        <img src={applogo} alt="space logo" width="38" height="38" />
+        <h2>Space Travelers Hub</h2>
       </div>
-      <nav />
+      <nav>
+        <li key={1}><NavLink to="/"> Rockets</NavLink></li>
+        <li key={2}><NavLink to="/Dragons"> Dragons</NavLink></li>
+        <li key={3}><NavLink to="/Missions"> Missions</NavLink></li>
+        <li key={4}><NavLink to="/Profile"> Profile</NavLink></li>
+      </nav>
     </div>
   );
 }
