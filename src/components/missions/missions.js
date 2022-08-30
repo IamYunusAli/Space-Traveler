09329197d3b30aa47f575557af395ccecd340063
sharon-odhiambo <88780311+sharon-odhiambo@users.noreply.github.com>
2022-08-30 14:26:51 +0300

    fix linter errors

--- src/components/missions/missions.js
+++ src/components/missions/missions.js
@@ -1,11 +1,10 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux/es/exports';
+import { useDispatch } from 'react-redux/es/exports';
 import { getMissions } from '../../redux/missions/mission';
 
 const Missions = () => {
-  const missions = useSelector((state) => state.missions);
+  // const missions = useSelector((state) => state.missions);
   const dispatch = useDispatch();
-  console.log(missions);
 
   useEffect(() => {
     dispatch(getMissions());
