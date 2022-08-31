9981d06586366a932a218edb347bba304cc84bbe
iamyunusali <aliyunus291@gmail.com>
2022-08-31 12:50:29 +0300

    Display reserved

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,6 +1,48 @@
-const Profile = () => (
-  <div>
-    <h1>Profile</h1>
-  </div>
-);
+import { useSelector } from 'react-redux';
+import './profile.css';
+
+const Profile = () => {
+  const dragons = useSelector((state) => state.dragon);
+  const reservedDr = dragons.filter((e) => e.reserved === true);
+  const displayDragons = () => {
+    if (reservedDr.length) {
+      return (
+        <ul>
+          { dragons.map((dragon) => (dragon.reserved === true ? (
+            <li>
+              {dragon.name}
+            </li>
+          ) : null))}
+        </ul>
+      );
+    }
+    return (<h2> 0 Reserved Dragons  </h2>);
+  };
+  return (
+    <div className="my-profile">
+      <div className="profiles">
+        <h2>Rockets</h2>
+        <div className="reserved">
+          {
+              }
+        </div>
+      </div>
+      <div className="profiles">
+        <h2>Missions</h2>
+        <div className="reserved">
+          {
+              }
+        </div>
+      </div>
+      <div className="profiles">
+        <h2>Dragons</h2>
+        <div className="reserved">
+          {
+           displayDragons()
+        }
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Profile;
