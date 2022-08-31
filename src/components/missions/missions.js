2c1011168d90bc9c1c9d87e4078b06c30b741399
sharon-odhiambo <88780311+sharon-odhiambo@users.noreply.github.com>
2022-08-31 22:57:35 +0300

    Use condition rendering for leave and join missions and display badges

--- src/components/missions/missions.js
+++ src/components/missions/missions.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux/es/exports';
 import { Table, Button } from 'react-bootstrap';
 import './missions.css';
-import { getMissions } from '../../redux/missions/mission';
+import { getMissions, joinMissions, leaveMissions } from '../../redux/missions/mission';
 
 const Missions = () => {
   const dispatch = useDispatch();
@@ -10,6 +10,16 @@ const Missions = () => {
   useEffect(() => {
     dispatch(getMissions());
   }, []);
+
+  const onClickJoinMissions = (e) => {
+    e.preventDefault();
+    dispatch(joinMissions(e.target.parentNode.parentNode.id));
+  };
+
+  const onClickLeaveMissions = (e) => {
+    e.preventDefault();
+    dispatch(leaveMissions(e.target.parentNode.parentNode.id));
+  };
 
   const missions = useSelector((state) => state.missions);
   return (
@@ -21,16 +31,23 @@ const Missions = () => {
             <th>Mission</th>
             <th>Description</th>
             <th>Status</th>
-            <th className="text-light">#</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
           {missions.map((mission) => (
-            <tr key={mission.mission_id}>
+            <tr key={mission.mission_id} id={mission.mission_id}>
               <td>{mission.mission_name}</td>
               <td>{mission.description}</td>
-              <td className="my-buttons"><Button>Not a member</Button></td>
-              <td className="my-buttons"><Button>Join mission</Button></td>
+              <td className="my-buttons">
+                {!mission.reserved && (<Button variant="secondary">NOT A MEMBER</Button>)}
+                {mission.reserved && (<Button>Active Member</Button>)}
+              </td>
+              <td className="my-buttons">
+                {!mission.reserved && (<Button variant="outline-secondary" onClick={onClickJoinMissions}>Join Mission</Button>)}
+                {mission.reserved && (<Button variant="outline-danger" onClick={onClickLeaveMissions}>Leave Mission</Button>
+                )}
+              </td>
             </tr>
           ))}
         </tbody>
--- src/redux/missions/mission.js
+++ src/redux/missions/mission.js
@@ -1,6 +1,8 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 const GET_MISSIONS = 'Space-Traveler/missions/GET_MISSIONS';
+const JOIN_MISSIONS = 'Space-Traveler/missions/JOIN_MISSIONS';
+const LEAVE_MISSIONS = 'Space-Traveler/missions/LEAVE_MISSIONS';
 
 export const getMissions = createAsyncThunk(GET_MISSIONS, async () => {
   const response = await fetch('https://api.spacexdata.com/v3/missions');
@@ -18,11 +20,35 @@ export const getMissions = createAsyncThunk(GET_MISSIONS, async () => {
   return arr;
 });
 
+export const joinMissions = (id) => ({
+  type: JOIN_MISSIONS,
+  payload: id,
+});
+
+export const leaveMissions = (id) => ({
+  type: LEAVE_MISSIONS,
+  payload: id,
+});
+
 const initialState = [];
 const missionsReducer = (state = initialState, action) => {
   switch (action.type) {
     case `${GET_MISSIONS}/fulfilled`:
       return action.payload;
+    case JOIN_MISSIONS:
+      return state.map((state) => {
+        if (state.mission_id === action.payload) {
+          return { ...state, reserved: true };
+        }
+        return state;
+      });
+    case LEAVE_MISSIONS:
+      return state.map((state) => {
+        if (state.mission_id === action.payload) {
+          return { ...state, reserved: false };
+        }
+        return state;
+      });
     default:
       return state;
   }
