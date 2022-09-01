1ec6e166b7165cb8819104e3b26197df4ee2ab1a
Pierre <piercel.coder2022@gmail.com>
2022-09-01 14:02:18 +0200

    Add action creators to the redux store

--- src/redux/rockets/rocketReduce.js
+++ src/redux/rockets/rocketReduce.js
@@ -1,4 +1,6 @@
 const FETCH_ROCKETS = 'FETCH_ROCKETS';
+const ADD_RESERVATION = 'ADD_RESERVATION';
+const CANCEL_RESERVATION = 'CANCEL_RESERVATION';
 const URL = 'https://api.spacexdata.com/v3/rockets';
 
 export const fetchRockets = async (dispatch) => {
@@ -7,11 +9,38 @@ export const fetchRockets = async (dispatch) => {
   dispatch({ type: FETCH_ROCKETS, payload: data });
 };
 
+export const addReservation = (id) => ({
+  type: ADD_RESERVATION,
+  id,
+});
+
+export const cancelReservation = (id) => ({
+  type: CANCEL_RESERVATION,
+  id,
+});
+
 const initstate = [];
 const rocketReduce = (state = initstate, action) => {
+  let newState;
   switch (action.type) {
     case FETCH_ROCKETS:
       return action.payload;
+    case ADD_RESERVATION:
+      newState = state.map((rocket) => {
+        if (rocket.id === action.id) {
+          return { ...rocket, reserved: true };
+        }
+        return rocket;
+      });
+      return newState;
+    case CANCEL_RESERVATION:
+      newState = state.map((rocket) => {
+        if (rocket.id === action.id) {
+          return { ...rocket, reserved: false };
+        }
+        return rocket;
+      });
+      return newState;
     default:
       return state;
   }
